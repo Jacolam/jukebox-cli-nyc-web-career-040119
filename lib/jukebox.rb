95233ac0eb4c2595ea51b5aabcf8e82db12efc6d
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-28 16:45:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,57 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end 
+
+def list(songs)
+  songs.each_with_index do |song , index|
+  puts "#{index +1}.#{song}"
+  end
+end
+
+def play(songs)
+binding.pry
+puts "Please enter a song name or number:"
+input = gets.chomp
+song_range = (1..songs.length)
+if songs.include?(input)
+  puts "Playing <#{input}>"
+  elsif song_range.include?(input)
+  puts "Playing <#{songs[input]}"
+else 
+  puts "Invalid input, please try again"
+end 
+
+end 
+
+
+def exit_jukebox
+puts "Goodbye"
+end 
+
+def run(songs)
+
+help # will run help method from before
+
+	Puts "Please enter a command:"
+	user_input = gets.chomp
+while user_input != exit
+	case choice
+		when user_input == "help"
+			help # run help method
+		when user_input == "play"
+			play # run play method
+		when user_input == "list"
+			list # run liste method 
+		end 
+		
+
+else
+	exit #run exit program , says goodbye and leaves loop
+end 
